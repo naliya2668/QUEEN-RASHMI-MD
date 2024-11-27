aebc3186897ead9e8c90ae30062da56133f0080d
QueenRashmiMD <wabot0999@gmail.com>
2024-11-27 10:54:58 +0530

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -1,50 +1,57 @@
-const {cmd , commands} = require('../command')
-const fg = require('dark-yasiya/yt-dl.js')
-const yts = require('yt-search')
+// YTMP3 DL PLUGIN
+
+const config = require('../config');
+const { cmd } = require('../command');
+const { ytsearch, ytmp3, ytmp4 } = require('@dark-yasiya/yt-dl.js'); // request package.json "@dark-yasiya/yt-dl.js": "latest"
+
+
 cmd({
-    pattern: "play2",
-    desc: "To download songs.",
-    react: "🎵",
+    pattern: "song",
+    alias: ["ytmp3","ytsong"],
+    react: "🎶",
+    desc: "Download Youtube song",
     category: "download",
+    use: '.song < Yt url or Name >',
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+async(conn, mek, m,{ from, prefix, quoted, q, reply }) => {
 try{
-if(!q) return reply("Please give me a url or title")  
-const search = await yts(q)
-const data = search.videos[0];
-const url = data.url
-    
-    
-let desc = `
-🎶 *QUEEN RASHMI MD WA-BOT SONG DOWNLOADER* 🎶
+
+if(!q) return await reply("Please give me Yt url or Name")
+	
+const yt = await ytsearch(q);
+if(yt.results.length < 1) return reply("Results is not found !")
+
+let yts = yt.results[0]  
+const ytdl = await ytmp3(yts.url)
+		
+let ytmsg = `
+🎶 QUEEN RASHMI MD WA-BOT SONG DOWNLOADER 🎶
 
 ┌───────────────────
-├ *ℹ️ Title:* ${yts.title}
-├ *📚 channel:* ${yts.author.name}
-├ *👁️‍🗨️ Views:* ${yts.views}
-├ *🕘 Duration:* ${yts.timestamp}
-├ *📆 Published:* ${yts.ago}
-├ *📥 Size:* ${yts.size}
-├ *🔗 Url:* ${yts.url}
+├ ℹ️ Title: ${yts.title}
+├ 📚 channel: ${yts.author.name}
+├ 👁️‍🗨️ Views: ${yts.views}
+├ 🕘 Duration: ${yts.timestamp}
+├ 📆 Published: ${yts.ago}
+├ 📥 Size: ${yts.size}
+├ 🔗 Url: ${yts.url}
 └───────────────────
 
-> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴍɪɴᴀ ᴏꜰᴄ ||*
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴍɪɴᴀ ᴏꜰᴄ ||
 `
+// SEND DETAILS
+await conn.sendMessage(from, { image: { url: yts.thumbnail || yts.image || '' }, caption: ${ytmsg}}, { quoted: mek });
 
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+// SEND AUDIO TYPE
+await conn.sendMessage(from, { audio: { url: ytdl.download.url }, mimetype: "audio/mpeg" }, { quoted: mek })
 
-//download audio
+// SEND DOC TYPE
+await conn.sendMessage(from, { document: { url: ytdl.download.url }, mimetype: "audio/mpeg", fileName: ytdl.result.title + '.mp3', caption: ${ytdl.result.title} }, { quoted: mek })
 
-let down = await fg.yta(url)
-let downloadUrl = down.dl_url
 
-//send audio message
-await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*© ᴄʀᴇᴀᴛᴇᴅ ʙʏ sɪʟᴇɴᴛ ʟᴏᴠᴇʀ⁴³²*"},{quoted:mek})
-
-}catch(e){
+} catch (e) {
 console.log(e)
-  reply('${e}')
-}
-})
+reply(e)
+}}
+)
